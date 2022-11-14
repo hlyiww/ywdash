87e5840a9b1364bbaa0e3deb06c5653d79d38b7a
yiwwhl <yiwwhl@qq.com>
2022-11-14 20:03:18 +0800

    chore: use tag version when ci

---
+++ build/index.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+const createPackageJson = () => `{
+    "name": "pandash",
+    "version": "${process.env.RELEASE_VERSION}",
+    "main": "index.cjs",
+    "module": "index.js",
+    "author": "yiwwhl",
+    "github": "https://github.com/hlyiww/pandash",
+    "description": "yiwwhl pandash",
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/hlyiww/pandash.git"
+    },
+    "keywords": ["工具"],
+    "license": "",
+    "bugs": {
+      "url": "https://github.com/hlyiww/pandash/issues"
+    },
+    "publishConfig": {
+      "registry": "https://registry.npmjs.org/",
+      "access": "public"
+    }
+  }`;
+
+const main = () => {
+  fs.writeFileSync("./dist/package.json", createPackageJson(), {
+    encoding: "utf8",
+  });
+};
+
+main();
