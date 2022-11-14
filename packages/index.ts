6ecbe3136476cce5deb94b11bd2f4bdb5c9c182d
yiwwhl <yiwwhl@qq.com>
2022-11-14 23:18:59 +0800

    feat: export keyBy

--- packages/index.ts
+++ packages/index.ts
@@ -2,12 +2,14 @@
 
 import compose from "@/compose/src";
 import groupBy from "@/groupBy/src";
+import keyBy from "@/keyBy/src";
 
 // TODO: auto export
 
 const _ = {
   compose,
   groupBy,
+  keyBy,
 };
 
 export default _;
