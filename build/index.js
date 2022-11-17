5c5d42e480fd9786ca628d0ba4849888648b9c46
yiwwhl <yiwwhl@qq.com>
2022-11-17 12:20:58 +0800

    chore: change project name

--- build/index.js
+++ build/index.js
@@ -1,21 +1,21 @@
 import fs from "fs";
 
 const createPackageJson = () => `{
-    "name": "pandash",
+    "name": "ywdash",
     "version": "${process.env.RELEASE_VERSION}",
     "main": "index.cjs",
     "module": "index.js",
     "author": "yiwwhl",
-    "github": "https://github.com/hlyiww/pandash",
+    "github": "https://github.com/hlyiww/ywdash",
     "description": "yiwwhl pandash",
     "repository": {
       "type": "git",
-      "url": "git+https://github.com/hlyiww/pandash.git"
+      "url": "git+https://github.com/hlyiww/ywdash.git"
     },
     "keywords": ["工具"],
     "license": "",
     "bugs": {
-      "url": "https://github.com/hlyiww/pandash/issues"
+      "url": "https://github.com/hlyiww/ywdash/issues"
     },
     "publishConfig": {
       "registry": "https://registry.npmjs.org/",
